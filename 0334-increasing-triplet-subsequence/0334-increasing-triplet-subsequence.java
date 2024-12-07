ad108e5e470fc9c1969ba0b2050ee2fe00314d5a
suri <suri.dev.kr@gmail.com>
2024-12-07 18:34:34 +0900

    Time: 2 ms (95.08%), Space: 134.2 MB (22.29%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,5 +1,14 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n <= min1)
+                min1 = n;
+            else if (n <= min2)
+                min2 = n;
+            else
+                return true;
+        }
+        return false;
     }
 }
